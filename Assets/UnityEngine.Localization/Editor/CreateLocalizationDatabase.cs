90491c11e538e7cad356fda017322e92bf66dfe1
Karl  Jones <karl@unity3d.com>
2017-05-25 20:58:08 +0200

    Added create localisation menu command. The old one does not work now it has been moved to UnityEngine.dll.

---
+++ Assets/UnityEngine.Localization/Editor/CreateLocalizationDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Localization;
+
+public class CreateLocalizationDatabase : MonoBehaviour 
+{
+	[MenuItem("Localization/Create Database")]
+	public static void CreateDB()
+	{
+		var path = EditorUtility.SaveFilePanelInProject("Create Localization Database", "Localization Database", "asset", "");
+		if (!string.IsNullOrEmpty (path)) 
+		{
+			var db = ScriptableObject.CreateInstance<MultiLangStringDatabase> ();
+			AssetDatabase.CreateAsset (db, path);
+		}
+	}
+}
