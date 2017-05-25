fb7eeb0fa35ea3fbfedb2bd065c8b247db9f0b6a
Stephen Palmer <stephenp@unity3d.com>
2017-05-25 10:11:36 +0200

    Setup market scene to load asset bundles for signage. Removed light mapping as it was way too dark.

---
+++ Assets/Scripts/AsyncLoader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ResourceManagement;
+
+public class AsyncLoader : MonoBehaviour {
+
+    [System.Serializable]
+    public struct LoadInfo
+    {
+        public string address;
+        public Transform parent;
+    }
+
+    public LoadInfo[] loadList;
+
+	void Start ()
+    {
+		foreach(var item in loadList)
+            ResourceManager.InstantiateAsync<GameObject>(item.address).complete += (obj) => {
+                obj.result.transform.SetParent(item.parent, false);
+            };
+	}
+}
