2995d80a19b5941c46169bbd8e680bdf1e4df0fa
Stephen Palmer <stephenp@unity3d.com>
2017-05-25 21:58:27 +0200

    Audio variants

---
+++ Assets/Scripts/CheckTagAndReloadAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTagAndReloadAudio : MonoBehaviour {
+
+    [SerializeField] string activeTag;
+
+	// Use this for initialization
+	void Start () {
+        activeTag = VirtualAssetManager.activeTag;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(VirtualAssetManager.activeTag != activeTag)
+        {
+            this.GetComponent<AudioSource>().Stop();
+            this.GetComponent<AudioSource>().Play();
+            this.activeTag = VirtualAssetManager.activeTag;
+        }
+	}
+}
