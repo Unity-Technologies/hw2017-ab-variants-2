72b534fbeea5beece5258023c65299317ffac3e2
Stephen Palmer <stephenp@unity3d.com>
2017-05-25 23:08:11 +0200

    Listen for language changes and update tag

--- Assets/Packages/VirtualAssets/LocalizationTagListener.cs
+++ Assets/Packages/VirtualAssets/LocalizationTagListener.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class LocalizationTagListener : MonoBehaviour
 {
+    void Start()
+    {
+        UpdateActiveTag(Application.currentLanguage.ToString());
+    }
+
     // Listen for language changes and update the current tag
     void OnApplicationLanguageChanged()
     {
-        Debug.Log ("Language changed to:" + Application.currentLanguage);
+        var newTag = Application.currentLanguage.ToString();
+        Debug.Log ("Language changed to:" + newTag);
+
+        UpdateActiveTag(newTag);
+    }
+
+    void UpdateActiveTag(string newTag)
+    {
+        // Hackweek!
+        if(newTag == "English")
+            newTag = "";
+        
+        VirtualAssetManager.activeTag = newTag;
+        VirtualAssetManager.ApplyRemap(newTag);
     }
 }
