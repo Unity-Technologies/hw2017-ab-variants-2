cfe0d0e03a631e7c1c7eba0e90e8ed97d5cc2033
Karl  Jones <karl@unity3d.com>
2017-05-25 22:44:40 +0200

    Added some runtime language support to office scene.

---
+++ Assets/UnityEngine.Localization/LanguageDb.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.Localization;
+
+public class LanguageDb : MonoBehaviour
+{
+	public MultiLangStringDatabase db;
+
+	void Awake()
+	{
+		db.MakeCurrent();
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(MultiLangStringDatabase.currentDb.ToString());
+	
+	}
+}
